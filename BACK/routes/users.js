0ebc920c76100098970883675af43fe60721c05a
fabrizioandrade <fabrizioandrade@icloud.com>
2023-07-13 00:14:09 -0300

    feature/components

--- BACK/routes/users.js
+++ BACK/routes/users.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const { createUser,loginUser,logOut } = require("../controllers/users");
+const validateUser = require("../middlewares/auth");
 const userRouter=express.Router()
 
 userRouter.post('/register',createUser)
 userRouter.post('/login',loginUser)
 userRouter.post('/logout',logOut)
-
+userRouter.get("/me", validateUser, (req, res) => {
+    res.status(200).send({ status: "OK", ...req.user });
+  });
 module.exports=userRouter;
--- FRONT/devFront/src/App.jsx
+++ FRONT/devFront/src/App.jsx
@@ -1,11 +1,13 @@
 import './App.css'
 import AuthWrapper from './common/AuthWrapper'
+import { Login } from './components/Login'
+import Register from './components/Register'
 
 function App() {
 
   return (
 < > 
-<AuthWrapper/>   
+<Register/>   
 </>
   )
 }
