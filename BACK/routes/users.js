30944dd7094245ae3ee9e1105b0e21ffbbb1fb28
fabrizioandrade <fabrizioandrade@icloud.com>
2023-07-14 11:38:58 -0300

    feature/validaciones

--- BACK/routes/users.js
+++ BACK/routes/users.js
@@ -1,9 +1,28 @@
 const express = require("express");
 const { createUser,loginUser,logOut } = require("../controllers/users");
 const validateUser = require("../middlewares/auth");
+const Joi=require('joi')
 const userRouter=express.Router()
 
-userRouter.post('/register',createUser)
+
+const registerSchema = Joi.object({
+  name: Joi.string().required(),
+  email: Joi.string().email().required(),
+  password: Joi.string().required(),
+  phone: Joi.string().required(),
+  code: Joi.string().allow(""),
+});
+
+userRouter.post('/register',(req, res, next) => {
+  // Valida los datos del registro usando el esquema
+  const { error } = registerSchema.validate(req.body);
+  if (error) {
+    // Si hay un error de validación, devuelve una respuesta de error
+    return res.status(400).send({ success: false, message: error.details[0].message });
+  }
+  // Si los datos son válidos, llama a la función createUser
+  next();
+},createUser)
 userRouter.post('/login',loginUser)
 userRouter.post('/logout',logOut)
 userRouter.get("/me", validateUser, (req, res) => {
