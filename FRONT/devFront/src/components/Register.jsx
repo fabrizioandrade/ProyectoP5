d0c333c38daa3d91f590cdf449c4ee4401f4138c
fabrizioandrade <fabrizioandrade@icloud.com>
2023-07-14 11:40:50 -0300

    fix/register

--- FRONT/devFront/src/components/Register.jsx
+++ FRONT/devFront/src/components/Register.jsx
@@ -71,7 +71,8 @@ Registrarse
               <form className="space-y-4 md:space-y-6" action="#" onSubmit={handleSubmit}>
               <div>
                   <label htmlFor="name" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Name</label>
-                  <input onChange={()=>setName(e.target.value)} type="text" name="name" id="name" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="username" required
+                  <input onChange={(e)=>setName(e.target.value)} type="text" name="name" id="name" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="username" 
+                  required
                   pattern={namePattern.source}
                   title='Por favor ingresa un nombre valido'/>
                 </div>
