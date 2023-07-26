ad717d60ab22b76dc4da41f0eac2ad4d691ca9cc
fabrizioandrade <fabrizioandrade@icloud.com>
2023-07-26 01:34:46 -0300

    fix favorite component

--- FRONT/devFront/src/components/Favorites.jsx
+++ FRONT/devFront/src/components/Favorites.jsx
@@ -212,7 +212,7 @@ const Favorites = () => {
                 </div>
                 <div className="self-center">
                 
-                  <button className="delete-favorite" onClick={() => removeFavorite(property.id)}
+                  <button  onClick={() => removeFavorite(property.id)}
                   
                   >
                     <svg
@@ -222,13 +222,11 @@ const Favorites = () => {
                       fill="none"
                       xmlns="http://www.w3.org/2000/svg"
                     >
-                      {/* Filled heart */}
                       <path
                         d="M2 9.1371C2 14 6.01943 16.5914 8.96173 18.9109C10 19.7294 11 20.5 12 20.5C13 20.5 14 19.7294 15.0383 18.9109C17.9806 16.5914 22 14 22 9.1371C22 4.27416 16.4998 0.825464 12 5.50063C7.50016 0.825464 2 4.27416 2 9.1371Z"
                         fill="#1C274C"
                         className="heart-filled"
                       />
-                      {/* Heart outline */}
                       <path
                         d="M2 9.1371C2 14 6.01943 16.5914 8.96173 18.9109C10 19.7294 11 20.5 12 20.5C13 20.5 14 19.7294 15.0383 18.9109C17.9806 16.5914 22 14 22 9.1371C22 4.27416 16.4998 0.825464 12 5.50063C7.50016 0.825464 2 4.27416 2 9.1371Z"
                         stroke="#1C274C"
